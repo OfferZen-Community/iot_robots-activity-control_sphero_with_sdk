5b4c00a9c398e324ca5f061963fd46b6efb798d3
Pedre <pviljoen@fullfacing.com>
2018-05-15 10:17:14 +0200

    Change color to green

--- run.js
+++ run.js
@@ -1,11 +1,11 @@
 //
-var sphero = require("sphero");
-var spheroId = process.argv[2];
-var orb = sphero(spheroId);
+var sphero = require("sphero")
+var spheroId = 'FD:94:C6:CA:0E:C0'
+var orb = sphero(spheroId)
 
 console.log('trying to connect to sphero...');
 
 orb.connect(function () {
   console.log('connected to sphero')
   orb.color("green");
-});
+})
