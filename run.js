03d0abb5891092e4bfa6bdf98db9aee6387ce897
Pedre <pviljoen@fullfacing.com>
2018-05-15 10:21:50 +0200

    Change color with delay

--- run.js
+++ run.js
@@ -3,9 +3,13 @@ var sphero = require("sphero")
 var spheroId = 'FD:94:C6:CA:0E:C0'
 var orb = sphero(spheroId)
 
-console.log('trying to connect to sphero...');
+console.log('trying to connect to sphero...')
 
 orb.connect(function () {
   console.log('connected to sphero')
-  orb.color("green");
+  orb.color("green")
+  //
+  setTimeout(function(){
+    orb.color("red")
+  }, 2000)
 })
