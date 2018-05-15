725d1c2a9f558466e9716991738eded22b605358
Pedre <pviljoen@fullfacing.com>
2018-05-15 10:39:25 +0200

    Move in square

--- run.js
+++ run.js
@@ -2,6 +2,7 @@
 var sphero = require("sphero")
 var spheroId = 'FD:94:C6:CA:0E:C0'
 var orb = sphero(spheroId)
+const turn = 90
 
 console.log('trying to connect to sphero...')
 
@@ -14,8 +15,16 @@ orb.connect(function () {
     return orb.color("green")
   })
 
-  orb.roll(100,0).delay(4000).then(() => {
-    console.log('Hello')
+  orb.roll(100,0).delay(1000).then(() => {
+    return orb.roll(100,turn)
+  }).delay(1000).then(() => {
+    return orb.roll(100, 2*turn)
+  }).delay(1000).then(() => {
+    return orb.roll(100, 3*turn)
+  }).delay(100).then(() => {
+    return orb.roll(100, 4*turn)
+  }).delay(1000).then(() => {
+    return orb.roll(0,0)
   })
   //
   
